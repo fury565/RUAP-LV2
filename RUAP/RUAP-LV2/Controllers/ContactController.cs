3e1ec17d9c5e15ca8bd8446c8936e45257503a75
Nikola <mdamjanovic2010@gmail.com>
2022-11-11 17:30:14 +0100

    Task 3 - Creating the Contact Modelsandchange Cont

--- RUAP/RUAP-LV2/Controllers/ContactController.cs
+++ RUAP/RUAP-LV2/Controllers/ContactController.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using RUAP_LV2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace RUAP_LV2.Controllers
 {
     public class ContactController : ApiController
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
+            return new Contact[]
             {
-                "Hello", "World"
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
             };
         }
     }
